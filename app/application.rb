85b62a26f6992138d6eb1ad61efa1261275b2240
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-07-29 17:13:06 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -14,27 +14,27 @@ class Application
         req = Rack::Request.new(env)
 
         if req.path.match(/items/)
+            # path = req.path
+            # binding.pry
             item_name = req.path.split("/items/").last 
-            binding.pry
-            if @@items.include?(item_name)
-                item = @@items.find{|i| i.name == item_name}
-                resp.write item.price
-            else
-                resp.write "Item not found"
-                resp.status = 400
-            end
+            check_item(item_name, resp)
         else
             resp.write "Route not found"
             resp.status = 404
         end# if ender
         resp.finish
-
-        
-        
-        
-        
-        
         
     end#def ender
+
+    def check_item(item, resp)
+
+        if found_item = @@items.find{|i| i.name == item}
+            resp.write found_item.price
+        else
+            resp.write "Item not found"
+            resp.status = 400
+        end
+    end
+
 
 end#class ender
