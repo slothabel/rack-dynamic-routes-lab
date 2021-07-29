51204d5986f717c074d6037ab8f3a6eea97558dd
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-07-29 17:27:38 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,9 +1,4 @@
-# #Instructions
-# Your application should only accept the /items/<ITEM NAME> route. Everything else should 404
 
-# If a user requests /items/<Item Name> it should return the price of that item
-
-# IF a user requests an item that you don't have, then return a 400 and an error message
 require 'pry'
 class Application 
 
@@ -14,8 +9,6 @@ class Application
         req = Rack::Request.new(env)
 
         if req.path.match(/items/)
-            # path = req.path
-            # binding.pry
             item_name = req.path.split("/items/").last 
             check_item(item_name, resp)
         else
